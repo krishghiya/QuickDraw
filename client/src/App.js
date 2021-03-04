a37943e31246b8a5d431d9269eaba580054729aa
Krish Ghiya <kghiya2@gmail.com>
2021-03-03 16:04:08 -0800

    Centered Canvas

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import CanvasDraw from 'react-canvas-draw'
 
 function App() {
   return (
-    <div className="App">
-        <p>
-          Edit
-        </p>
+    <div className="container">
+        <h1>
+          Welcome to Quick Draw!
+        </h1>
+        <CanvasDraw style={{
+          position: 'absolute', left: '50%', top: '50%',
+          transform: 'translate(-50%, -50%)'}}/>
     </div>
   );
 }
