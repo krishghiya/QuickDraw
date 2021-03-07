2acde137fd7612e810d896bdf5885ffe76b10eac
Krish Ghiya <kghiya2@gmail.com>
2021-03-06 20:21:09 -0800

    Key

--- server/web_project/settings.py
+++ server/web_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sstegt$%3$78ogxe&5wn1rt94hbchz-ac(fbjeq3_p85yn&=nb'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
