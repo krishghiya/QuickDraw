1c34d2bc592d5b9c4dd01364f8dfc96cf6e6a4a4
Krish Ghiya <kghiya2@gmail.com>
2021-03-06 20:20:14 -0800

    Finished basic stack

--- client/src/App.js
+++ client/src/App.js
@@ -2,42 +2,60 @@ import CanvasDraw from 'react-canvas-draw'
 import {useState} from 'react'
 import axios from 'axios'
 
-function sendData(data) {
-  axios.post('http://localhost:8000/data/', {
-    lines: data
-  }, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then(res => {
-    console.log(res);
-    return res;
-  })
-}
+const defaultList = [
+  {
+    name: 'No prediction', 
+    confidence: '100',
+  }]
 
 function App() {
 
+  async function sendData(data) {
+    try {
+      const res = await axios.post('http://localhost:8000/data/', {
+        lines: data
+        }, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }).then(res => setList(res['data']))
+      return res;
+    }catch (error) {
+      setList(defaultList)  
+    }
+  }
+
   const [color, setColor] = useState('black')
   const [saveableCanvas, setsaveableCanvas] = useState(null)
-
+  const [list, setList] = useState(defaultList)
+  
   return (
     <div className="container">
         <h1>
           Welcome to Quick Draw!
         </h1>
-        <CanvasDraw 
-          className='canvas'
-          ref={canvasDraw => setsaveableCanvas(canvasDraw)}
-          onChange={() => setColor("#" + Math.floor(Math.random() * 16777215).toString(16))}
-          brushColor={color}
-          hideGrid={true}
-          brushRadius={2}
-          lazyRadius={0}
-        />
-        <div className='button'>
-          <button onClick={() => sendData(saveableCanvas.getSaveData())}>Predict Doodle</button>
-          <button onClick={() => saveableCanvas.clear()}>Clear Screen</button>
-        </div>
+          <CanvasDraw 
+            className='canvas'
+            ref={canvasDraw => setsaveableCanvas(canvasDraw)}
+            onChange={() => {
+              setColor("#" + Math.floor(Math.random() * 16777215).toString(16));
+              sendData(saveableCanvas.getSaveData());
+            }}
+            brushColor={color}
+            hideGrid={true}
+            brushRadius={2}
+            lazyRadius={0}
+          />
+          <ol className='results'>
+            {list.map((item) => (
+            <li key={item.name}>
+              <p>Resembles <b>{item.name}</b> with confidence: {item.confidence}%</p>
+            </li>
+            ))}
+          </ol>
+        <button className='button' onClick={() => {saveableCanvas.clear();setList(defaultList);}}>
+            Clear Screen
+        </button>
     </div>
   );
 }
--- server/hello/views.py
+++ server/hello/views.py
@@ -2,10 +2,17 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 import numpy as np
-# import tensorflow as tf
+import pickle
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+import tensorflow as tf
+
+model = tf.keras.models.load_model(os.path.split(os.getcwd())[0] + '\model')
+names = pickle.load(open('hello/names.pickle', 'rb'))
 
 def home(request):
-    print(request)
     return HttpResponse("Hello, Django!")
 
 @csrf_exempt
@@ -18,7 +25,58 @@ def predict(request):
             for d in l:
                 points.append([d['x'], d['y'], 0])
             points[-1][-1] = 1
+        
+        points = np.array(points)
+        # initial_plot_image(points)
 
-        return HttpResponse("Success!")
+         # Preprocessing.
+         # 1. Size normalization.
+        lower = np.min(points[:, 0:2], axis=0)
+        upper = np.max(points[:, 0:2], axis=0)
+        scale = upper - lower
+        scale[scale == 0] = 1
+        points[:, 0:2] = (points[:, 0:2] - lower) / scale
+        # 2. Compute deltas.
+        points[1:, 0:2] -= points[0:-1, 0:2]
+        np_ink = points[1:, :].reshape(1, -1, 3)
+
+        # plot_image(np_ink[0])
+
+        results = model.predict(np_ink)
+        values, indices = tf.nn.top_k(results[0], k=3)
+        indices = indices.numpy() 
+        indices = [names[i] for i in indices]
+        values = values.numpy() * 100
+        str_values = [{'name': x.title(), 'confidence': str(round(y, 2))} 
+                    for x, y in zip(indices, values)]
+
+        return HttpResponse(json.dumps(str_values))
     else:
         return HttpResponse("GET Request invalid!") 
+
+def plot_image(ink):
+    import matplotlib.pyplot as plt
+    origin_points = [[0., 0., 0.]]
+    points = np.r_[origin_points, ink]
+    line_indices = np.argwhere(points[:, -1])[:,0]
+    coordinates = np.cumsum(points[:, :2], axis=0)
+    strokes = np.split(coordinates, line_indices + 1)
+    plt.scatter(coordinates[:, 0], -coordinates[:, 1], marker='.')
+    
+    for stroke in strokes:
+        plt.plot(stroke[:, 0], -stroke[:, 1])
+
+    plt.axis("off")
+    plt.show()
+
+def initial_plot_image(points):
+    import matplotlib.pyplot as plt
+    line_indices = np.argwhere(points[:, -1])[:,0]
+    strokes = np.split(points[:, :2], line_indices + 1)
+    plt.scatter(points[:, 0], -points[:, 1])
+
+    for s in strokes:
+        plt.plot(s[:, 0], -s[:, 1])
+    
+    plt.axis('off')
+    plt.show()
